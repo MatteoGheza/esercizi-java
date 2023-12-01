e44672111a64bb31bc5befa626d707b4d7eaaccd
Matteo Gheza <matteo.gheza07@gmail.com>
2023-12-01 09:52:04 +0200

    Update Es2B

--- Es2B/src/main/java/com/mycompany/es2b/Es2B.java
+++ Es2B/src/main/java/com/mycompany/es2b/Es2B.java
@@ -15,10 +15,28 @@ public class Es2B {
         Giocatore topolino = new Giocatore("TopoDisney", "Malefico", 95, "portiere", 515, 58);
         Giocatore pluto = new Giocatore();
         Giocatore pippo = new Giocatore("Pippo", "Bianchi", 56, "attaccante", 24, 2);
-        Giocatore matteo = new Giocatore("Matteo", "Gheza", 16);
         
-        System.out.println(topolino.nome);
-        System.out.println(pluto.cognome);
-        System.out.println(pluto.nome);
+        System.out.println("Nome di topolino: " + topolino.nome);
+        System.out.println("Nome di pluto: " + pluto.cognome);
+        System.out.println("Nome di pippo: " + pippo.nome);
+        
+        topolino.nome = "Topolino";
+        System.out.println("Nome di topolino: " + topolino.nome);
+        
+        pippo.setEta(19);
+        System.out.println("Età di pippo: " + pippo.getEta());
+        
+        Giocatore matteo = new Giocatore("Matteo", "Ghezza", 16);
+        System.out.println("Età di matteo: " + matteo.getEta());
+        matteo.setEta(17);
+        System.out.println("Età di matteo: " + matteo.getEta());
+        
+        System.out.println("Ruolo di matteo: " + matteo.getRuolo());
+        matteo.setRuolo("Panchinaro");
+        System.out.println("Ruolo di matteo: " + matteo.getRuolo());
+        
+        System.out.println("Cognome di matteo: " + matteo.cognome);
+        matteo.cognome = "Gheza";
+        System.out.println("Cognome di matteo: " + matteo.cognome);
     }
 }
